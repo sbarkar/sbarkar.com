c78ee8f7aae2358fb1defb9e471f1f94bcf6d624
Sergej Barkar <sergej.barkar+github@gmail.com>
2024-04-29 09:49:06 +0000

    vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 import React from "react";
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
